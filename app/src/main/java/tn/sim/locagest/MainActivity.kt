0d95711f4f3572d0cfa97d64b5183efcce90eddf
Marwen-Hammami <hammamimarwen.d@gmail.com>
2023-11-03 18:27:36 +0100

    architecture

---
+++ app/src/main/java/tn/sim/locagest/MainActivity.kt
@@ -0,0 +1,11 @@
+package tn.sim.locagest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/tn/sim/locagest/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package tn.sim.locagest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tn.sim.locagest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
